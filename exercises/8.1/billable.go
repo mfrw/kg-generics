5e23a3da5366b80813f9bc36f717478ca8a2a088
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-09-25 12:45:36 +0530

    billable: implement generic channels
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- exercises/8.1/billable.go
+++ exercises/8.1/billable.go
@@ -1,3 +1,35 @@
 package billable
 
+import "sync/atomic"
+
 // Your implementation of Channel goes here!!
+
+type Channel[T any] struct {
+	ch chan T
+	tx atomic.Int32
+	rx atomic.Int32
+}
+
+func NewChannel[T any](capacity int) *Channel[T] {
+	return &Channel[T]{
+		ch: make(chan T, capacity),
+	}
+}
+
+func (c *Channel[T]) Send(e T) {
+	defer func() { c.tx.Add(1) }()
+	c.ch <- e
+}
+
+func (c *Channel[T]) Receive() T {
+	defer func() { c.rx.Add(1) }()
+	return <-c.ch
+}
+
+func (c *Channel[T]) Sends() int {
+	return int(c.tx.Load())
+}
+
+func (c *Channel[T]) Receives() int {
+	return int(c.rx.Load())
+}
