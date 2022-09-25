74cca17580714887d194204efd101b03194f3076
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-09-25 12:06:34 +0530

    stack: impl a generic Stack
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- exercises/7.1/stack.go
+++ exercises/7.1/stack.go
@@ -1,3 +1,24 @@
 package stack
 
 // Your implementation of Stack goes here!
+
+type Stack[T any] []T
+
+func (s *Stack[T]) Push(t ...T) {
+	*s = append(*s, t...)
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	var elt T
+	var ok bool
+	if len(*s) != 0 {
+		elt = (*s)[len(*s)-1]
+		*s = (*s)[:len(*s)-1]
+		ok = true
+	}
+	return elt, ok
+}
+
+func (s *Stack[T]) Len() int {
+	return len(*s)
+}
