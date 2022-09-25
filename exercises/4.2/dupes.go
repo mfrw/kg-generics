f66a79a98a1f5fd0954e1b31b92b9c12044bcd01
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-09-25 08:44:08 +0530

    dupes: implement a generic map to check for dupes
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- exercises/4.2/dupes.go
+++ exercises/4.2/dupes.go
@@ -1,3 +1,14 @@
 package dupes
 
 // Your Dupes function goes here!
+
+func Dupes[T comparable](s []T) bool {
+	mp := make(map[T]struct{})
+	for _, v := range s {
+		if _, ok := mp[v]; ok {
+			return true
+		}
+		mp[v] = struct{}{}
+	}
+	return false
+}
