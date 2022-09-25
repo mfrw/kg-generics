e1da333d6e78c14c3fb2057f038416152e62c9bc
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-09-25 07:41:41 +0530

    initish: impl Initish by stealing constraints from stdlib
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- exercises/3.2/intish.go
+++ exercises/3.2/intish.go
@@ -1,6 +1,17 @@
 package intish
 
 // Your Intish interface goes here!
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+type Intish interface {
+	Signed | Unsigned
+}
 
 func IsPositive[T Intish](v T) bool {
 	return v > 0
