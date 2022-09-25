d49de72754225cf4a8df0f4afa937507eca1eabe
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-09-25 07:06:28 +0530

    print: impl PrintAnythingTo basic solution
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- exercises/2.1/print.go
+++ exercises/2.1/print.go
@@ -1,3 +1,12 @@
 package print
 
+import (
+	"fmt"
+	"io"
+)
+
 // Your PrintAnythingTo function goes here!
+
+func PrintAnythingTo[T any](buf io.Writer, val T) {
+	buf.Write([]byte(fmt.Sprintln(val)))
+}
