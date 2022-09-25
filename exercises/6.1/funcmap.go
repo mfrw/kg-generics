c379d683ffb049513d7456e7a46c4ca178b4d0aa
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-09-25 10:41:18 +0530

    funcmap: impl generic Apply for Input/Output generic params
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- exercises/6.1/funcmap.go
+++ exercises/6.1/funcmap.go
@@ -1,3 +1,13 @@
 package funcmap
 
 // Your implementation of FuncMap goes here!
+
+type FuncMap[I any, O any] map[string]func(i I) O
+
+func (fm FuncMap[I, O]) Apply(fn string, i I) O {
+	var def O
+	if f, ok := fm[fn]; ok {
+		return f(i)
+	}
+	return def
+}
