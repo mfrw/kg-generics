e02d2e5c0cdc41cdc80774b785e7af5ebd43e5c0
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-09-25 13:04:00 +0530

    merge: impl Merge using a dumb for range
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- exercises/9.2/merge.go
+++ exercises/9.2/merge.go
@@ -1,5 +1,14 @@
 package merge
 
-import "golang.org/x/exp/maps"
-
 // Your implementation of Merge goes here!
+
+func Merge[M ~map[K]V, K comparable, V any](mps ...M) M {
+	mp := map[K]V{}
+
+	for _, m := range mps {
+		for k, v := range m {
+			mp[k] = v
+		}
+	}
+	return mp
+}
